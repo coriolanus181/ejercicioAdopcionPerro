cb54d0e907cf98f45e71cd3e04ad59ae5211bcc7
kevinSantos <kevinlich180@gmail.com>
2025-04-21 09:50:37 -0500

    beta

---
+++ src/Persona.java
@@ -0,0 +1,61 @@
+import java.util.List;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String documento;
+    private List<Perro> listperro;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, int edad, String documento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "documento='" + documento + '\'' +
+                ", edad=" + edad +
+                ", apellido='" + apellido + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
